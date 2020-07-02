7ff7e737efb5670123470985bebe9ee42e2273ac
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-02 16:24:18 -0600

    create poros

---
+++ app/poros/propublica_service.rb
@@ -0,0 +1,12 @@
+class PropublicaService
+  def members_of_house(state)
+    response = conn.get("/congress/v1/members/house/#{state}/current.json")
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: 'https://api.propublica.org') do |f|
+      f.headers['X-API-Key'] = ENV['PROPUBLICA_API_KEY']
+    end
+  end
+end
